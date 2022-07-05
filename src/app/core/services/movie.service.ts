88341c988924b51b3782ca390d174f294badd9ca
Sirius Zhang <sirius.zhang997@gmail.com>
2022-07-05 13:27:42 -0700

    added services

---
+++ src/app/core/services/movie.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+
+// HomeComponent will call this method
+  getTopGrossingMovies() {
+
+  }
+
+  getMovieDetails(id: number) {
+
+  }
+
+  getMovieByGenre(genreId: number) {
+
+  }
+}
